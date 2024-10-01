0f2e8ed8c5de93a5d972064cb4081ce9e2e7ab4e
FelipeLancho <felipemarin46@gmail.com>
2024-09-30 21:49:27 -0300

    Tela esqueci a minha senha

--- src/app/esqueci-senha/esqueci-senha.component.ts
+++ src/app/esqueci-senha/esqueci-senha.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-esqueci-senha',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './esqueci-senha.component.html',
   styleUrl: './esqueci-senha.component.css'
 })
 export class EsqueciSenhaComponent {
+  public mensagem: String = "";
+  public obj: Cliente = new Cliente();
 
+  public enviarSenha(){
+    this.mensagem="Um email de recuperação de senha foi enviado, verifique sua caixa de entrada!"
+  }
 }
